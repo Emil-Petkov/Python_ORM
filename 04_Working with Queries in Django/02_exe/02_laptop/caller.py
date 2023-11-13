16f68ce16919ef33db1c83103ee1cfc6ececa8f9
EmilPetkov1723 <114022856+Emil-Petkov@users.noreply.github.com>
2023-11-13 19:58:09 +0200

    .

--- 04_Working with Queries in Django/02_exe/02_laptop/caller.py
+++ 04_Working with Queries in Django/02_exe/02_laptop/caller.py
@@ -1,24 +1,23 @@
 import os
 import django
 
+# Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
+# Import your models
 from main_app.models import ArtworkGallery, Laptop
-from django.db.models import Q, Case, When, Value, F
 
 
+# Create and check models
 def show_highest_rated_art():
-    best_artwork = ArtworkGallery.objects.order_by("-rating", "id").first()
+    art = ArtworkGallery.objects.all().order_by("-rating", "id").first()
 
-    return f"{best_artwork.art_name} is the highest-rated art with a {best_artwork.rating} rating!"
+    return f"{art.art_name} is the highest-rated art with a {art.rating} rating!"
 
 
 def bulk_create_arts(first_art, second_art):
-    ArtworkGallery.objects.bulk_create([
-        first_art,
-        second_art,
-    ])
+    ArtworkGallery.objects.bulk_create([first_art, second_art])
 
 
 def delete_negative_rated_arts():
@@ -26,9 +25,9 @@ def delete_negative_rated_arts():
 
 
 def show_the_most_expensive_laptop():
-    best_laptop = Laptop.objects.order_by("-price", "id").first()
+    laptop = Laptop.objects.all().order_by('-price', 'id').first()
 
-    return f"{best_laptop.brand} is the most expensive laptop available for {best_laptop.price}$!"
+    return f"{laptop.brand} is the most expensive laptop available for {laptop.price}$!"
 
 
 def bulk_create_laptops(*args):
@@ -36,44 +35,20 @@ def bulk_create_laptops(*args):
 
 
 def update_to_512_GB_storage():
-    # Laptop.objects.filter(
-    #     brand__in=[
-    #         "Asus",
-    #         "Lenovo"
-    #     ]
-    # ).update(storage=512)
-
-    Laptop.objects.filter(Q(brand="Asus") | Q(brand="Lenovo")).update(storage=512)
+    Laptop.objects.filter(brand__in=["Asus", "Lenovo"]).update(storage=512)
 
 
 def update_to_16_GB_memory():
-    # Laptop.objects.filter(
-    #     brand__in=[
-    #         "Apple",
-    #         "Dell",
-    #         "Acer"
-    #     ]
-    # ).update(storage=16)
-
-    Laptop.objects.filter(Q(brand="Apple") | Q(brand="Dell") | Q(brand="Acer")).update(memory=16)
+    Laptop.objects.filter(brand__in=["Apple", "Dell", "Acer"]).update(memory=16)
 
 
 def update_operation_systems():
-    Laptop.objects.update(
-        operation_system=Case(
-            When(brand="Asus", then=Value("Windows")),
-            When(brand="Apple", then=Value("MacOS")),
-            When(brand__in=["Acer", "Dell"], then=Value("Linux")),
-            When(brand="Lenovo", then=Value("Chrome OS")),
-            default=F("operation_system"),
-        )
-    )
-
-    # Laptop.objects.filter(brand="Asus").update(operation_systems="Windows")
-    # Laptop.objects.filter(brand="Apple").update(operation_systems="MacOS")
-    # Laptop.objects.filter(brand__in=["Dell", "Acer"]).update(operation_systems="Linux")
-    # Laptop.objects.filter(brand="Lenovo").update(operation_systems="Chrome OS")
+    Laptop.objects.filter(brand="Asus").update(operation_system="Windows")
+    Laptop.objects.filter(brand="Apple").update(operation_system="MacOS")
+    Laptop.objects.filter(brand__in=["Dell", "Acer"]).update(operation_system="Linux")
+    Laptop.objects.filter(brand="Lenovo").update(operation_system="Chrome OS")
 
 
 def delete_inexpensive_laptops():
     Laptop.objects.filter(price__lt=1200).delete()
+
